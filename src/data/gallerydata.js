e06712b7688c836ebdb960dd0ecf05c84664421c
softleek-official <softleeklimited@gmail.com>
2025-02-20 22:23:12 +0300

    gallery

--- src/data/gallerydata.js
+++ src/data/gallerydata.js
@@ -1,12 +1,12 @@
 const galleryData = [
-  { id: 11, image: "/images/gallery/11.jpg", title: "Elegant Living Room" },
-  { id: 12, image: "/images/gallery/12.jpg", title: "Modern Kitchen" },
-  { id: 13, image: "/images/gallery/13.jpg", title: "Cozy Bedroom" },
-  { id: 14, image: "/images/gallery/14.jpg", title: "Luxury Bathroom" },
-  { id: 15, image: "/images/gallery/15.jpg", title: "Stylish Office Space" },
-  { id: 16, image: "/images/gallery/16.jpg", title: "Outdoor Patio" },
-  { id: 17, image: "/images/gallery/17.jpg", title: "Classic Dining Area" },
-  { id: 18, image: "/images/gallery/18.jpg", title: "Spacious Backyard" },
+  { id: 11, image: "/images/gallery/31.jpg", title: "Elegant Living Room" },
+  { id: 12, image: "/images/gallery/32.jpg", title: "Modern Kitchen" },
+  { id: 13, image: "/images/gallery/33.jpg", title: "Cozy Bedroom" },
+  { id: 14, image: "/images/gallery/34.jpg", title: "Luxury Bathroom" },
+  { id: 15, image: "/images/gallery/35.jpg", title: "Stylish Office Space" },
+  { id: 16, image: "/images/gallery/36.jpg", title: "Outdoor Patio" },
+  { id: 17, image: "/images/gallery/37.jpg", title: "Classic Dining Area" },
+  { id: 18, image: "/images/gallery/38.jpg", title: "Spacious Backyard" },
 ];
 
 export default galleryData;
